f0ddf2f4062776d5d22e0c0a160970dd973db8f5
Mark Hanna <mark.hanna.fawzy@gmail.com>
2018-04-27 19:52:36 +0200

    adding core data aand alamofire

--- PopularMovies/PopularMovies/controllers/HomePageCollectionView.swift
+++ PopularMovies/PopularMovies/controllers/HomePageCollectionView.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
-
+import Alamofire
+import SDWebImage
+import SwiftyJSON
+import CoreData
 private let reuseIdentifier = "homeCollectionCell"
 
 class MovieCollectionCell: UICollectionViewCell {
@@ -18,82 +21,251 @@ class MovieCollectionCell: UICollectionViewCell {
 }
 
 class HomePageCollectionView: UICollectionViewController {
-
+    
+    var arrRes = [[String:AnyObject]]() //Array of dictionary
+    var arrTrailerRes = [[String:AnyObject]]()
+    var arrReviewRes = [[String:AnyObject]]()
+    var arrResFromCoreData = [NSManagedObject]() //Array of ManagedObjects
+    var flag : Bool = true
+    var tempImg : UIImage!
+    var tempImgView : UIImageView!
+    var rowIndex : Int!
+    var destinationVC : MovieDetailsViewController!
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = false
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tempImg = UIImage()
+        tempImgView = UIImageView(image: tempImg)
+        Alamofire.request("http://api.themoviedb.org/3/discover/movie?api_key=bd97fe04de1096c3c59c20c445de2b05").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                if let resData = swiftyJsonVar["results"].arrayObject {
+                    self.arrRes = resData as! [[String:AnyObject]]
+                    //print(self.arrRes)
+                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                    let managedContext = appDelegate.persistentContainer.viewContext
+                    let request = NSFetchRequest<NSManagedObject>(entityName: "Movie")
+                    do {
+                        try self.arrResFromCoreData = managedContext.fetch(request)
+                    } catch {
+                        print("error")
+                    }
+                    for i in 0..<self.arrResFromCoreData.count
+                    {
+                        managedContext.delete(self.arrResFromCoreData[i])
+                    }
+                    do {
+                        try managedContext.save()
+                    } catch {
+                        print("error")
+                    }
+                }
+                else{
+                    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                    let managedContext = appDelegate.persistentContainer.viewContext
+                    let request = NSFetchRequest<NSManagedObject>(entityName: "Movie")
+                    do {
+                        try self.arrResFromCoreData = managedContext.fetch(request)
+                    } catch {
+                        print("error")
+                    }
+                    self.flag = false
+                    self.collectionView?.reloadData()
+                }
+                if self.arrRes.count > 0 {
+                    self.collectionView?.reloadData()
+                }
+            }
+        }
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
     // MARK: UICollectionViewDataSource
-
+    
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
-
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 10
+        return arrRes.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MovieCollectionCell
-    
-        // Configure the cell
-    
+        if flag
+        {
+            var dict = arrRes[indexPath.row]
+            
+            var imgUrl = dict["poster_path"] as? String
+            var fullUrl = "http://image.tmdb.org/t/p/w185/"+imgUrl!
+            
+            //cell.poster_image_view.sd_setImage(with: URL(string: fullUrl), placeholderImage: UIImage(named: "placeholder.png"))
+            cell.poster_image_view.image = UIImage(named: "star_wars.jpg")
+            cell.movie_name.text = dict["title"] as? String
+            
+            var img2Url = dict["backdrop_path"] as? String
+            var full2Url = "http://image.tmdb.org/t/p/w185/"+img2Url!
+            //tempImgView.sd_setImage(with: URL(string: full2Url), placeholderImage: UIImage(named: "placeholder.png"))
+            tempImgView.image = UIImage(named: "star_wars.jpg")
+            
+            var appDelegate = UIApplication.shared.delegate as! AppDelegate
+            var managedContext = appDelegate.persistentContainer.viewContext
+            
+            var entity = NSEntityDescription.entity(forEntityName: "Movie" , in: managedContext)
+            
+            var newMovie = NSManagedObject(entity: entity!, insertInto: managedContext)
+            
+            var imgData = UIImageJPEGRepresentation(cell.poster_image_view.image!, 1)
+            newMovie.setValue(imgData, forKey: "posterPath2")
+            
+            var img2Data = UIImageJPEGRepresentation((tempImgView.image)!, 1)
+            newMovie.setValue(img2Data, forKey: "backdropPath2")
+            
+            newMovie.setValue(dict["id"]as? Int, forKey: "id")
+            newMovie.setValue(dict["title"] as? String, forKey: "originalTitle")
+            newMovie.setValue(dict["overview"]as? String, forKey: "overview")
+            newMovie.setValue(dict["release_date"]as? String, forKey: "releaseDate")
+            
+            var movieId  = dict["id"] as? Int
+            var reviewURL = "http://api.themoviedb.org/3/movie/\(movieId)/reviews?api_key=bd97fe04de1096c3c59c20c445de2b05"
+            
+            self.getReviewData(activeGenreLink: reviewURL, completion: {
+                //updateUI
+                var authorFull : String = ""
+                var contentFull : String = ""
+                for i in 0..<self.arrReviewRes.count
+                {
+                    var dictReview = self.arrReviewRes[i]
+                    authorFull = authorFull + (dictReview["author"] as? String)! + "#"
+                    contentFull = contentFull + (dictReview["content"] as? String)! + "#"
+                }
+                
+                newMovie.setValue(authorFull, forKey: "reviewAuthor")
+                newMovie.setValue(contentFull, forKey: "reviewContent")
+            })
+            //            Alamofire.request("http://api.themoviedb.org/3/movie/\(movieId)/reviews?api_key=bd97fe04de1096c3c59c20c445de2b05").responseJSON { (responseData) -> Void in
+            //                if((responseData.result.value) != nil) {
+            //                    let swiftyJsonVar = JSON(responseData.result.value!)
+            //
+            //                    if let resData = swiftyJsonVar["results"].arrayObject {
+            //                        self.arrReviewRes = resData as! [[String:AnyObject]]
+            //                        var authorFull : String = ""
+            //                        var contentFull : String = ""
+            //                        for i in 0..<self.arrReviewRes.count
+            //                        {
+            //                            var dictReview = self.arrReviewRes[i]
+            //                            authorFull = authorFull + (dictReview["author"] as? String)! + "#"
+            //                            contentFull = contentFull + (dictReview["content"] as? String)! + "#"
+            //                        }
+            //
+            //                        newMovie.setValue(authorFull, forKey: "reviewAuthor")
+            //                        newMovie.setValue(contentFull, forKey: "reviewContent")
+            //                    }
+            //                }
+            //            }
+            
+            //            Alamofire.request("http://api.themoviedb.org/3/movie/\(movieId)/videos?api_key=bd97fe04de1096c3c59c20c445de2b05").responseJSON { (responseData) -> Void in
+            //                if((responseData.result.value) != nil) {
+            //                    let swiftyJsonVar = JSON(responseData.result.value!)
+            //
+            //                    if let resData = swiftyJsonVar["results"].arrayObject {
+            //                        self.arrTrailerRes = resData as! [[String:AnyObject]]
+            //                        var dict2 = self.arrTrailerRes[1]
+            //
+            //                        newMovie.setValue(dict2["key"] as? String, forKey: "trailerKey")
+            newMovie.setValue("awaaa", forKey: "trailerKey")
+            //                    }
+            //                }
+            //            }
+            
+            
+            newMovie.setValue(dict["vote_average"] as? Float, forKey: "rating")
+            
+            print ("Data added")
+            do {
+                try managedContext.save()
+            } catch {
+                print("error")
+            }
+        }
+        else
+        {
+            cell.poster_image_view.image = UIImage(data: arrResFromCoreData[indexPath.row].value(forKey: "posterPath2") as! Data)
+            cell.movie_name.text = arrResFromCoreData[indexPath.row].value(forKey: "originalTitle") as! String
+        }
         return cell
     }
-
+    func getReviewData(activeGenreLink: String,completion : @escaping ()->()){
+        
+        Alamofire.request(activeGenreLink).responseJSON { responseData in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                if let resData = swiftyJsonVar["results"].arrayObject {
+                    self.arrReviewRes = resData as! [[String:AnyObject]]
+                }
+            }
+            completion()
+        }//End of Json request
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        self.destinationVC = segue.destination as! MovieDetailsViewController
+    }
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.rowIndex = indexPath.row
+        destinationVC.movieIndex = self.rowIndex
+    }
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
+    /*
+     // Uncomment this method to specify if the specified item should be highlighted during tracking
+     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment this method to specify if the specified item should be selected
+     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+     return true
+     }
+     */
+    
+    /*
+     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+     return false
+     }
+     
+     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+     
+     }
+     */
 
 }
--- PopularMovies/PopularMovies/controllers/MovieDetailsViewController.swift
+++ PopularMovies/PopularMovies/controllers/MovieDetailsViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import CoreData
 class MovieDetailsViewController: UITableViewController {
     
     @IBOutlet weak var details_large_poster: UIImageView!
@@ -18,10 +18,23 @@ class MovieDetailsViewController: UITableViewController {
     @IBOutlet weak var details_movie_overview: UILabel!
     @IBOutlet weak var details_moview_reviews: UITextView!
     
-
+    var movieIndex : Int!
+    var arrResFromCoreData = [NSManagedObject]() //Array of ManagedObjects
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSManagedObject>(entityName: "Movie")
+        do {
+            try self.arrResFromCoreData = managedContext.fetch(request)
+        } catch {
+            print("error")
+        }
+        details_small_poster.image = UIImage(data: arrResFromCoreData[movieIndex].value(forKey: "posterPath2") as! Data)
+        details_large_poster.image = UIImage(data: arrResFromCoreData[movieIndex].value(forKey: "backdropPath2") as! Data)
+        details_movie_title.text = arrResFromCoreData[movieIndex].value(forKey: "originalTitle") as! String
+        details_movie_overview.text = arrResFromCoreData[movieIndex].value(forKey: "overview") as! String
+        details_moview_reviews.text = arrResFromCoreData[movieIndex].value(forKey: "reviewContent") as! String
         self.tabBarController?.tabBar.isHidden = true
 
         // Uncomment the following line to preserve selection between presentations
@@ -54,6 +67,15 @@ class MovieDetailsViewController: UITableViewController {
     
     @IBAction func displayTrailer(_ sender: UIButton) {
         //call youtube code
+        var movieKey = arrResFromCoreData[movieIndex].value(forKey: "trailerKey") as! String
+        if let youtubeURL = URL(string: "youtube://\(movieKey)"),
+            UIApplication.shared.canOpenURL(youtubeURL) {
+            // redirect to app
+            UIApplication.shared.open(youtubeURL, options: [:], completionHandler: nil)
+        } else if let youtubeURL = URL(string: "https://www.youtube.com/watch?v=\(movieKey)") {
+            // redirect through safari
+            UIApplication.shared.open(youtubeURL, options: [:], completionHandler: nil)
+        }
     }
     
 
